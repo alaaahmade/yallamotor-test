1ca70528b84a031fb37b66f7bd06ea3ea7ecb734
alaaahmade <alaaahmade66@gmail.com>
2025-07-18 00:40:15 +0300

    enhance Performance

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 import type { NextConfig } from "next";
+import createBundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,4 +15,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
