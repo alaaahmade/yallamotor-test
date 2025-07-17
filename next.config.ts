7f770a25b98f51128a0c792f40f457a8c879bf57
alaaahmade <alaaahmade66@gmail.com>
2025-07-17 09:07:51 +0300

    main branch

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["ymimg1.b8cdn.com", "yallamotor-production-assets.s3.ap-south-1.amazonaws.com"],
+  },
 };
 
 export default nextConfig;
